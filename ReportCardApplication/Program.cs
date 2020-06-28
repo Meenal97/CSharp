299ed6b15b967a91291eef75fd63c511b55cabe3
Meenal97 <meenalpreet94@gmail.com>
2020-06-28 21:45:39 +0530

    Sum and avg

--- ReportCardApplication/Program.cs
+++ ReportCardApplication/Program.cs
@@ -10,7 +10,6 @@ namespace ReportCardApplication
             int number;
             Console.WriteLine("Enter number of Students");
             number = Convert.ToInt32(Console.ReadLine());
-
             string[] name = new string[number];
             int[,] marks = new int[number, 3];
             for (int i = 0; i < number; i++)
@@ -29,12 +28,18 @@ namespace ReportCardApplication
             /* Data */
             for (int i = 0; i < number; i++)
             {
+                int sum = 0;
+                float avg = 0.0f;
                 Console.WriteLine(name[i]);
                 for(int j=0;j<3;j++)
                 {
                     Console.Write("Marks in Subject {0}{1} ", i,j);
                     Console.WriteLine( marks[i,j]);
+                    sum += marks[i, j];
                 }
+                Console.WriteLine("Sum {0} ", sum);
+                avg = sum / 3;
+                Console.WriteLine("Average {0} ", avg);
             }
         }
     }
