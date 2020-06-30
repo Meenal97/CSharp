7871085f2c09505f3f143a5f99e5c2088aef824f
Meenal97 <meenalpreet94@gmail.com>
2020-06-30 18:39:18 +0530

    Access Specifires

---
+++ AccessSpecifier/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AccessSpecifier
+{
+    class Access
+    {
+        private int num = 100;
+        public int num2 = 200;
+        public int Number()
+        {
+            return num;
+        }
+        public void Method()
+        {
+            Console.WriteLine("A public Method");
+        }
+
+        public static void Method2()
+        {
+            Console.WriteLine("A public Method 2");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Access a = new Access();
+            a.Method();
+            Access.Method2();
+            Console.WriteLine(a.Number());
+            Console.WriteLine(a.num2 + 100);
+        }
+    }
+}
