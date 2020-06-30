19004e75829d3bfc07d45e9f15985411fd9855e2
Meenal97 <meenalpreet94@gmail.com>
2020-06-30 21:17:02 +0530

    Access Specifier

--- AccessSpecifier/Program.cs
+++ AccessSpecifier/Program.cs
@@ -1,11 +1,13 @@
-﻿using System;
+﻿using AccessSpecifier;
+using System;
 
 namespace AccessSpecifier
 {
-    class Access
+     class Access
     {
         private int num = 100;
         public int num2 = 200;
+        protected int num3 = 500;
         public int Number()
         {
             return num;
@@ -20,6 +22,32 @@ namespace AccessSpecifier
             Console.WriteLine("A public Method 2");
         }
     }
+
+    class Child : Access
+    {
+        public int protected_method()
+        {
+            return num3;
+        }
+
+        public void protected_method2()
+        {
+            Console.WriteLine("Num: {0}", num3 * 6);
+        }
+    }
+
+    class Two : Access
+    {
+        public int protected_method()
+        {
+            return num2;
+        }
+
+        public void protected_method2()
+        {
+            Console.WriteLine("Num: {0}", num2 * 6);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
@@ -27,8 +55,24 @@ namespace AccessSpecifier
             Access a = new Access();
             a.Method();
             Access.Method2();
+
             Console.WriteLine(a.Number());
             Console.WriteLine(a.num2 + 100);
+
+            Child c = new Child();
+            Console.WriteLine(c.protected_method());
+
+            c.protected_method();
+            c.protected_method2();
+
+            Two t = new Two();
+            Console.WriteLine(c.protected_method());
+
+            t.protected_method();
+            t.protected_method2();
         }
     }
 }
+
+
+
