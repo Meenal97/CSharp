6ca4ffc7496173295607727498b286cffc26b1f0
Meenal97 <meenalpreet94@gmail.com>
2020-07-05 16:07:48 +0530

    library

--- CountOnes/Program.cs
+++ CountOnes/Program.cs
@@ -6,25 +6,11 @@ namespace CountOnes
     {
         static void Main(string[] args)
         {
-            int m, count = 0;
-            Console.WriteLine("Enter the Limit : ");
-            m = int.Parse(Console.ReadLine());
-            int[] a = new int[m];
-            Console.WriteLine("Enter the Numbers :");
-            for (int i = 0; i < m; i++)
-            {
-                a[i] = Convert.ToInt32(Console.ReadLine());
-            }
-            foreach (int o in a)
-            {
-                if (o == 1)
-                {
-                    count++;
-                }
-            }
-            Console.WriteLine("Number of 1's in the Entered Number : ");
-            Console.WriteLine(count);
-            Console.ReadLine();
+            string str = "11441155";
+            int num = Convert.ToInt32(str);
+            Console.WriteLine(num);
+            int[] arr = num
+            
         }
     }
 }
