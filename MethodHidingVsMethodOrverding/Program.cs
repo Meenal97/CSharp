6c3164583cae61ab50ce80c5d306062e5169d829
Meenal97 <meenalpreet94@gmail.com>
2020-07-10 23:23:18 +0530

    MethodOverrdiding vd hiding

---
+++ MethodHidingVsMethodOrverding/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodHidingVsMethodOrverding
+{
+    public class Employee
+    {
+        public void Print()
+        {
+            Console.WriteLine("I am in Base class");
+        }
+        public virtual void Hello()
+        {
+            Console.WriteLine("Hello, I am in Base class");
+        }
+    }
+
+
+    public class PartTimeEmployee : Employee
+    {
+        public new void Print()
+        {
+            Console.WriteLine("I am in Derived class");
+        }
+
+        public override void Hello()
+        {
+            Console.WriteLine("Hello, I am in Derived class");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PartTimeEmployee PTE = new PartTimeEmployee();
+            PTE.Print();
+            PTE.Hello();
+
+            Console.WriteLine("________________________________________________________");
+
+            Employee E = new PartTimeEmployee();
+            E.Print();
+            E.Hello();
+
+            Console.ReadKey();
+        }
+    }
+}
