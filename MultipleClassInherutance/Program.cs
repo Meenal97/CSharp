894d2de428870e2407a79de92c60a7449c57916d
Meenal97 <meenalpreet94@gmail.com>
2020-07-12 14:05:15 +0530

    Multiple Inhertance using interface

---
+++ MultipleClassInherutance/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultipleClassInherutance
+{
+    interface IA
+    {
+        void AMethod();
+    }
+    class A : IA
+    {
+        public void AMethod()
+        {
+            Console.WriteLine("A");
+        }
+    }
+
+    interface IB
+    {
+        void BMethod();
+    }
+    class B : IB
+    {
+        public void BMethod()
+        {
+            Console.WriteLine("B");
+        }
+    }
+
+    class AB : IA, IB
+    {
+        A a = new A();
+        B b = new B();
+        public void AMethod()
+        {
+            a.AMethod();
+        }
+        public void BMethod()
+        {
+            b.BMethod();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AB abc = new AB();
+            abc.AMethod();
+
+
+            Console.ReadKey();
+        }
+    }
+}
