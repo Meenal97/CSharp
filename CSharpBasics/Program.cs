f249e37a7688f6775e8f0dc4e06b5f0ff0d96cf3
Meenal97 <meenalpreet94@gmail.com>
2020-07-14 22:46:19 +0530

    InnerException

---
+++ CSharpBasics/Program.cs
@@ -0,0 +1,18 @@
+﻿using CSharpBasics.TryCatch;
+using System;
+
+namespace CSharpBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            ExceptionHandling e1 = new ExceptionHandling();
+            e1.Numbers();
+
+     
+        }
+    }
+}
